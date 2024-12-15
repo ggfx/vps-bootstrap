24ee310a3aeb5b63acae911725fdd1c9c5ccfb78
Cornelius <golney@gmx.net>
2024-12-15 11:48:02 +0100

    Add vps-bootstrap shell script

---
+++ vps-bootstrap.sh
@@ -0,0 +1,470 @@
+#!/bin/bash
+# Debian VPS Bootstrap
+# 2024-12-15
+# Cornelius Rittner
+# Get Variables
+export HOSTNAME=$(hostname -f)
+export PUBLIC_IPV4=$(hostname -I | awk '{print $1}')
+#project=${HOSTNAME%-*}
+echo "------------------------------------------------------------"
+echo Hostname: $HOSTNAME, IP Address: $PUBLIC_IPV4
+echo "------------------------------------------------------------"
+
+# first Update and Upgrade all packages
+apt-get update && apt-get -y upgrade
+apt-get -y install apt-transport-https ssl-cert net-tools apache2-utils curl sudo logrotate
+
+# Install Munin-Node (maybe replace with Prometheus)
+#apt-get -y install munin-node libwww-perl python3-docker libcache-cache-perl libdbd-mysql-perl
+# no libdbd-mariadb-perl pkg on Ubuntu 22.04
+#apt-get -y libdbd-mariadb-perl
+# add allowed server ip
+#if [ $(egrep -L "^allow \^92\\\.51" /etc/munin/munin-node.conf) ]
+#then
+#  sed -i '/^allow \^::1\$/a allow ^92\\.51\\.162\\.6$' /etc/munin/munin-node.conf
+#fi
+#service munin-node restart
+
+# Other Basics
+apt-get -y install vim htop zip ca-certificates mailutils gnupg locate rsync unattended-upgrades wget bind9-dnsutils
+
+# set tzdata to Europa/Berlin
+ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+dpkg-reconfigure --frontend noninteractive tzdata
+# set locale to de_DE.UTF-8
+DEBIAN_FRONTEND=noninteractive apt-get install -y locales
+sed -i -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen && echo 'LANG="de_DE.UTF-8"'>/etc/default/locale
+dpkg-reconfigure --frontend noninteractive locales && update-locale LANG=de_DE.UTF-8
+
+# Install certbot
+if [ ! -x "$(command -v snap)" ]; then
+  apt-get -y install snapd
+  snap install core; snap refresh core
+fi
+if [ ! -x "$(command -v certbot)" ]; then
+  snap install --classic certbot
+  ln -fs /snap/bin/certbot /usr/bin/certbot
+fi
+
+# Install postfix as standard smtpd
+DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
+
+# set vim colors and dircolors
+if [ ! -f ~/.vimrc ]; then
+  echo 'syntax on
+  colorscheme desert' > ~/.vimrc
+elif [ $(grep -L "syntax on" ~/.vimrc) ]; then
+  echo 'syntax on
+  colorscheme desert' >> ~/.vimrc
+fi
+
+# set directory colorization
+dircolors -p > ~/.dircolors
+sed -i 's/^DIR [0-9;]\+ # directory$/DIR 00;34;47 # directory/' ~/.dircolors
+eval "$(dircolors -b ~/.dircolors)"
+
+# create confirmation scripts, added in .bash_aliases
+echo 'echo ""
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!! Attention PROD system !!!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo ""
+echo ""
+echo -n "Run the command \"$@\" now? "
+echo -n "(y/n) "
+read reply
+
+if [ "$reply" = y ]
+then
+   /sbin/$@
+else
+   echo "\"$@\" canceled"
+fi' > ~/confirm_script
+chmod +x ~/confirm_script
+
+# set .bash_aliases
+if [ ! -f ~/.bash_aliases ]; then
+  echo "# ~/.bash_aliases: sourced by bashrc.
+# colorize the prompt
+PS1='\${debian_chroot:+(\$debian_chroot)}\[\[\033[1;33m\]\t:\[\033[1;31m\]\u\[\033[1;36m\]@\h\[\e[0m\]:\w]\\\$ '
+
+# colorize directory listing
+test -r ~/.dircolors && eval \"\$(dircolors -b ~/.dircolors)\" || eval \"\$(dircolors -b)\"
+export LS_OPTIONS='--color=auto'
+alias ls='ls \$LS_OPTIONS'
+
+# listing abbreviations
+alias ll='ls -l'
+alias la='ll -a'
+alias lh='ll -h'
+alias lah='la -h'
+alias l='lh -A'
+alias elsa='ls -lSah'
+
+# traversing abbreviations
+alias ..='cd ..'
+alias ...='.. && ..'
+
+# alias to confim commands
+alias shutdown='~/confirm_script shutdown'
+alias reboot='~/confirm_script reboot'" > ~/.bash_aliases
+elif [ $(grep -L "alias to confirm" ~/.bash_aliases) ]; then
+  echo "# ~/.bash_aliases: sourced by bashrc.
+# colorize the prompt
+PS1='\${debian_chroot:+(\$debian_chroot)}\[\[\033[1;33m\]\t:\[\033[1;31m\]\u\[\033[1;36m\]@\h\[\e[0m\]:\w]\\\$ '
+
+# colorize directory listing
+test -r ~/.dircolors && eval \"\$(dircolors -b ~/.dircolors)\" || eval \"\$(dircolors -b)\"
+export LS_OPTIONS='--color=auto'
+alias ls='ls \$LS_OPTIONS'
+
+# listing abbreviations
+alias ll='ls -l'
+alias la='ll -a'
+alias lh='ll -h'
+alias lah='la -h'
+alias l='lh -A'
+alias elsa='ls -lSah'
+
+# traversing abbreviations
+alias ..='cd ..'
+alias ...='.. && ..'
+
+# alias to confim commands
+alias shutdown='~/confirm_script shutdown'
+alias reboot='~/confirm_script reboot'" >> ~/.bash_aliases
+fi
+
+if [ $(grep -L "if \[ -f ~\/\.bash_aliases \];" ~/.bashrc) ]
+then
+  echo -e "\n# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+if [ -f ~/.bash_aliases ]; then
+  . ~/.bash_aliases
+fi" >> ~/.bashrc
+fi
+
+source ~/.bash_aliases
+source ~/.bashrc
+
+# Install dynamic motd messages for login
+apt-get -y install toilet figlet
+
+# clear default motd
+echo "" > /etc/motd
+
+cat <<EOF >/etc/update-motd.d/00-header
+#!/bin/sh
+#
+#    00-header - create the header of the MOTD
+#    Copyright (C) 2023 Cornelius Rittner
+#    Copyright (C) 2009-2010 Canonical Ltd.
+#
+#    Authors: Cornelius Rittner <rittner@wirth-horn.de>
+#             Dustin Kirkland <kirkland@canonical.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+[ -r /etc/lsb-release ] && . /etc/lsb-release
+
+if [ -z "\$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
+        # Fall back to using the very slow lsb_release utility
+        DISTRIB_DESCRIPTION=\$(lsb_release -s -d)
+fi
+
+toilet -F gay -f small \$(hostname -s)
+printf "\n"
+
+printf "Welcome to %s (%s %s %s)\n" "\$DISTRIB_DESCRIPTION" "\$(uname -o)" "\$(uname -r)" "\$(uname -m)"
+printf "\n"
+
+toilet -F border -f term FQDN: \$(hostname -f)
+printf "\n"
+EOF
+chmod +x /etc/update-motd.d/00-header
+
+cat <<EOF >/etc/update-motd.d/01-sysinfo
+#!/bin/bash
+#
+#    01-sysinfo - generate the system information
+#    Copyright (C) 2023 Cornelius Rittner
+#
+#    Authors: Cornelius Rittner <rittner@wirth-horn.de>
+#
+
+sysinfo(){
+  if [ -x /usr/bin/landscape-sysinfo ]; then
+# this is for Ubuntu
+# don't try refresh this more than once per minute
+# Due to cpu consumption and login delays (LP: #1893716)
+stamp="/var/lib/landscape/landscape-sysinfo.cache"
+NEED_UPDATE="FALSE"
+[ -z "\$(find "\$stamp" -newermt 'now-1 minutes' 2> /dev/null)" ] && NEED_UPDATE="TRUE"
+
+if [ "\$NEED_UPDATE" = "TRUE" ]; then
+    # pam_motd does not carry the environment
+    [ -f /etc/default/locale ] && . /etc/default/locale
+    export LANG
+    cores=\$(grep -c ^processor /proc/cpuinfo 2>/dev/null)
+    [ "\$cores" -eq "0" ] && cores=1
+    threshold="\${cores:-1}.0"
+    if [ \$(echo "`cut -f1 -d ' ' /proc/loadavg` < \$threshold" | bc) -eq 1 ]; then
+        printf "\n\033[01m  System-Status (`/bin/date`)\033[0m\n\033[1;30m  ==================================================================\033[0m\n%s\n\033[1;30m  ==================================================================\033[0m\n" \\
+            "\$(/usr/bin/landscape-sysinfo | sed '/Graph/d' | sed '/https/d' | sed '/^\s*$/d')" \\
+#        printf "\n  System information as of %s\n\n%s\n" \\
+#            "\$(/bin/date)" \\
+#            "\$(/usr/bin/landscape-sysinfo)" \\
+            > "\$stamp"
+    else
+        # do not replace a formerly good result due to load
+        if ! grep -q "System information as of" \$stamp 2> /dev/null; then
+            printf "\n System information disabled due to load higher than %s\n" "\$threshold" > "\$stamp"
+        fi
+    fi
+fi
+
+[ ! -r "\$stamp" ] || cat "\$stamp"
+  else
+# this is for Debian
+    user=\$(whoami)
+    lastlog=\`lastlog -u \${user} | grep -v Latest |  awk '{ printf \$5" "\$6" "\$7" "\$8" "\$9" from "\$3 }'\`
+    date=\`date\`
+    load=\`cat /proc/loadavg | awk '{print \$1" (1minute) "\$2" (5minutes) "\$3" (15minutes)"}'\`
+    root_usage=\`df -h / | awk '/\// { percent=\$(NF-1); total=\$2 } END { print percent,"of",total }'\`
+    root_used=\`df -h / | awk '/\// {print \$(NF-3)}'\`
+    root_total=\`df -h / | awk '/\// {print \$(NF-2)}'\`
+    memory_usage=\`free -m | awk '/(Speicher|Mem):/ { total=\$2; used=\$3 } END { printf("%3.1f%%", "exit !total;used/total*100") }'\`
+    memory=\`free -h | awk '/Mem|Speicher/ { print \$2 }'\`
+    memory_used=\`free -h | awk '/Mem|Speicher/ { print \$3 }'\`
+
+    swap_usage=\`free -m | awk '/Swap:/ { total=\$2; used=\$3 } END { printf("%3.1f%%", "exit !total;used/total*100") }'\`
+    users=\`users | wc -w\`
+    time=\`uptime | grep -ohe 'up .*' | sed 's/,/\ hours/g' | awk '{ printf \$2" "\$3 }'\`
+    processes_total=\`ps aux | wc -l\`
+    processes_user=\`ps -U \${user} u | wc -l\`
+
+    #ip=\`ifconfig \$(route | grep default | awk '{ print \$8 }') | grep "inet" | awk '{print \$2}'\`
+    ip=\`hostname -I | awk '{print \$1}'\`
+
+    printf "\n\033[01m  System-Status (`/bin/date`)\033[0m\n\033[1;30m  ==================================================================\033[0m\n"
+    echo -e "  Last Login:\t\t\$lastlog"
+#       printf "  System load:\t\t%s\n  Usage of /:\t\t%s\n" \$load \$root_usage
+    echo -e "  System load:\t\t\$load\n  Usage on /:\t\t\$root_used/\$root_total (\$root_usage)"
+    printf "  Memory Usage:\t\t%s\n  Swap Usage:\t\t%s\n" \$memory_usage \$swap_usage
+#       printf "  Usage On /:\t%s\tSwap Usage:\t%s\n" \$root_usage \$swap_usage
+    printf "  Processes:\t\t%s\n  Users logged in:\t%s\n" "\$processes_total total, \$processes_user yours" \$users
+    printf "  System Uptime:\t%s\n" "\$time"
+    printf "  IP Address:\t\t%s\n" \$ip
+    printf "\033[1;30m  ==================================================================\033[0m\n"
+  fi
+}
+
+sysinfo
+EOF
+chmod +x /etc/update-motd.d/01-sysinfo
+
+cat <<EOF >/etc/update-motd.d/02-updates
+#!/bin/bash
+#
+#    02-updates - generate the update information
+#    Copyright (C) 2023 Cornelius Rittner
+#
+#    Authors: Cornelius Rittner <rittner@wirth-horn.de>
+#
+
+updates(){
+    if [ -x /usr/lib/update-notifier/update-motd-updates-available ]; then
+        echo -e "\n\033[01m  Update Status:\033[0m"
+        echo -e "\033[1;30m  ==================================================================\033[0m"
+        stamp="/var/lib/update-notifier/updates-available"
+        [ ! -r "\$stamp" ] || cat "\$stamp" | sed 's/^/  /g' | sed '/^\s*$/d'
+        find \$stamp -newermt 'now-7 days' 2> /dev/null | grep -q -m 1 '.' || /usr/share/update-notifier/notify-updates-outdated | sed 's/^/  /g' | sed '/^\s*$/d'
+        echo -e "\033[1;30m  ==================================================================\n\033[0m"
+    else
+        echo -e "\n\033[01m  Update Status:\033[0m"
+        echo -e "\033[1;30m  ==================================================================\033[0m"
+        echo -e "  \033[0;33m\$(apt list --upgradable 2>/dev/null | sed -E '/^(Auflistung.*|Listing.*)?$/d' | wc -l)\033[0m packages can be updated."
+        echo -e "  \033[1;31m\$(apt list --upgradable 2>/dev/null | grep "\-security" | wc -l)\033[0m are security updates."
+        echo -e "\033[1;30m  ==================================================================\n\033[0m"
+    fi
+}
+
+updates
+EOF
+chmod +x /etc/update-motd.d/02-updates
+
+# remove default motd 50-landscape-sysinfo -> /usr/share/landscape/landscape-sysinfo.wrapper
+if [ -f /etc/update-motd.d/50-landscape-sysinfo ]; then
+    rm /etc/update-motd.d/50-landscape-sysinfo
+fi
+# disable others
+if [ -f /etc/update-motd.d/10-help-text ]; then
+    chmod -x /etc/update-motd.d/10-help-text
+fi
+if [ -f /etc/update-motd.d/50-motd-news ]; then
+    chmod -x /etc/update-motd.d/50-motd-news
+fi
+if [ -f /etc/update-motd.d/90-updates-available ]; then
+    chmod -x /etc/update-motd.d/90-updates-available
+fi
+if [ -f /etc/update-motd.d/10-uname ]; then
+    chmod -x /etc/update-motd.d/10-uname
+fi
+
+# Install IP Tables Firewall, use legacy version
+DEBIAN_FRONTEND=noninteractive apt-get install -y iptables
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+# create firewall rules
+cat <<EOF >/etc/iptables.rules
+*filter
+# Create new chain
+-N DOCKER-USER
+
+# Allows all loopback (lo0) traffic and drop all traffic to 127/8 that doesn't use lo0
+-A INPUT -i lo -j ACCEPT
+-A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT
+
+# Accepts all established inbound connections
+-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allows all outbound traffic
+# You could modify this to only allow certain traffic
+-A OUTPUT -j ACCEPT
+
+# Allows HTTP and HTTPS connections from anywhere (the normal ports for websites)
+-A INPUT -p tcp --dport 80 -j ACCEPT
+-A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allow FTP connections from anywhere
+-A INPUT -p tcp --dport 21 -j ACCEPT
+-A INPUT -p tcp --match multiport --dports 50000:50100 -j ACCEPT
+
+# Allows SSH connections
+# The --dport number is the same as Port in /etc/ssh/sshd_config
+-A INPUT -s 80.151.193.21,93.104.244.81,83.169.19.191 -p tcp -m state --state NEW --dport 666 -j ACCEPT
+
+# Allows Munin connections
+-A INPUT -s 92.51.162.6 -p tcp --dport 4949 -j ACCEPT
+
+# Allow ISPConfig connections
+#-A INPUT -s 80.151.193.21,93.104.244.81 -p tcp -m state --state NEW --dport 8080 -j ACCEPT
+# Allow Docker Portainer
+#-A INPUT -s 80.151.193.21,93.104.244.81 -p tcp -m state --state NEW --dport 9443 -j ACCEPT
+
+# Allow Nydus (Hosteurope)
+-A INPUT -p tcp --dport 2224 -j ACCEPT
+
+# Allows Postfix Traffic
+#-A INPUT -p tcp --dport 25 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+# Now you should read up on iptables rules and consider whether ssh access
+# for everyone is really desired. Most likely you will only allow access from certain IPs.
+
+# Allow ping
+#  note that blocking other types of icmp packets is considered a bad idea by some
+#  remove -m icmp --icmp-type 8 from this line to allow all kinds of icmp:
+#  https://security.stackexchange.com/questions/22711
+-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
+
+# log iptables denied calls (access via 'dmesg' command)
+-A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+
+# Reject all other inbound - default deny unless explicitly allowed policy:
+-A INPUT -j REJECT
+# Append forward to DOCKER-USER chain before REJECT
+-A FORWARD -j DOCKER-USER
+-A FORWARD -j REJECT
+
+# Append DOCKER-USER chain filters
+# Docker does the port-mapping in the PREROUTING chain of the nat table. This happens before the filter rules,
+# so --dest and --dport will see the internal IP and port of the container.
+# To access the original destination, you can use -m conntrack --ctorigdstport
+# Allow Docker Portainer
+#-A DOCKER-USER -s 80.151.193.21,93.104.244.81 -p tcp -m conntrack --ctorigdstport 9443 --ctdir ORIGINAL -j ACCEPT
+# Drop all other traffic to Portainer
+#-A DOCKER-USER -p tcp -m conntrack --ctorigdstport 9443 --ctdir ORIGINAL -j DROP
+-A DOCKER-USER -j RETURN
+
+COMMIT
+EOF
+
+cat <<EOF >/etc/ip6tables.rules
+*filter
+
+# Allows all loopback (lo0) traffic and drop all traffic to ::1/128 that doesn't use lo0
+-A INPUT -i lo -j ACCEPT
+-A INPUT ! -i lo -d ::1/128 -j REJECT
+
+# Accepts all established inbound connections
+-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allows all outbound traffic
+# You could modify this to only allow certain traffic
+-A OUTPUT -j ACCEPT
+
+# Allows HTTP and HTTPS connections from anywhere (the normal ports for websites)
+#-A INPUT -p tcp --dport 80 -j ACCEPT
+#-A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allow FTP connections from anywhere
+#-A INPUT -p tcp --dport 21 -j ACCEPT
+#-A INPUT -p tcp --match multiport --dports 50000:50100 -j ACCEPT
+
+# Allow Nydus (Hosteurope)
+-A INPUT -p tcp --dport 2224 -j ACCEPT
+
+# Allow ping
+#  note that blocking other types of icmp packets is considered a bad idea by some
+#  remove -m icmp --icmp-type 8 from this line to allow all kinds of icmp:
+#  https://security.stackexchange.com/questions/22711
+-A INPUT -p ipv6-icmp --icmpv6-type 128 -j ACCEPT
+
+# log ip6tables denied calls (access via 'dmesg' command)
+-A INPUT -m limit --limit 5/min -j LOG --log-prefix "ip6tables denied: " --log-level 7
+
+# Reject all other inbound - default deny unless explicitly allowed policy:
+-A INPUT -j REJECT
+-A FORWARD -j REJECT
+
+COMMIT
+EOF
+
+echo "--------------------------------------------------"
+echo "Virtual Private Server Bootstrap finished"
+echo "Restart your terminal session (logout and relogin)"
+echo "--------------------------------------------------"
+
+if [ $(grep -L "Port 52022" /etc/ssh/sshd_config) ]; then
+    echo -e "\nYou should change your sshd_config and set the SSH Port to 52022 and PermitRootLogin prohibit-password:"
+    echo "vi /etc/ssh/sshd_config"
+    echo "Afterwards restart the ssh service:"
+    echo "systemctl restart ssh"
+    echo -e "\nTwo firewall rulesets were created in /etc/iptables.rules and /etc/ip6tables.rules."
+    echo "Change them as required and then load it for IPv4:"
+    echo "iptables-restore < /etc/iptables.rules"
+    echo "For IPv6 use:"
+    echo "ip6tables-restore < /etc/ip6tables.rules"
+    echo -e "\nTo make the rules persistent on boot-time install iptables-persistent:"
+    echo "apt-get install iptables-persistent"
+    echo -e "\nYou can save your rules manually or change them directly in the persisten file:"
+    echo "iptables-save > /etc/iptables/rules.v4"
+    echo "For IPv6 please use:"
+    echo "ip6tables-save > /etc/iptables/rules.v6"
+fi
